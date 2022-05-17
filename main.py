94fd15aba77f0f950c2d0094886b6f7d38b04cba
goonmandu <hwang.mingi7@gmail.com>
2022-05-17 19:02:53 -0400

    implement custom type enums

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
+from enum import Enum
+
+
+class Material(Enum):
+    HENTAI = 1
+    IRL = 2
+    RENDER = 3
+    ASMR = 4
+
+
+class Parties(Enum):
+    MALE_MALE = 1
+    MALE_FEMALE = 2
+    FEMALE_FEMALE = 3
+    SOLO_MALE = 4
+    SOLO_FEMALE = 5
+    OTHER = 6
+
+
 class Fap:
-    def __init__(self, year, month, day, hour, minute):
+    def __init__(self, year, month, day, hour, minute, duration, material, parties):
         self.year = year
         self.month = month
         self.day = day
         self.hour = hour
         self.minute = minute
+        self.duration = duration
+        self.material = material
+        self.parties = parties
 
 
-event = Fap(2022, 5, 18, 17, 32)
-
-print(event.year, event.month, event.day, event.hour, event.minute)
-
+with open("data", "r+") as data:
+    fap_array = []
